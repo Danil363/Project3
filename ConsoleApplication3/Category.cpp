5bd7daadabda8eeb95b21159faa92fa5028379ec
Denis4423 <den00741@gmail.com>
2025-01-31 09:15:54 +0200

    Delete ConsoleApplication3 directory

--- ConsoleApplication3/Category.cpp
+++
@@ -1,7 +0,0 @@
-#include "Category.h"
-
-Category::Category(const std::string& name) : name(name) {}
-
-std::string Category::getName() const {
-    return name;
-}
--- ConsoleApplication3/Discount.cpp
+++
@@ -1,7 +0,0 @@
-#include "Discount.h"
-
-Discount::Discount(double discountPercentage) : discountPercentage(discountPercentage) {}
-
-double Discount::applyDiscount(double price) const {
-    return price * (1 - discountPercentage / 100);
-}
